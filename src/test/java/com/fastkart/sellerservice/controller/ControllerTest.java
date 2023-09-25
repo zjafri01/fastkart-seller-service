3859d74ff05e870a4e9a79474e0bfc06b7feba6a
zjafri01 <zjafri@deloitte.com>
2023-09-25 15:51:45 +0530

    Seller Service Tests

---
+++ src/test/java/com/fastkart/sellerservice/controller/ControllerTest.java
@@ -0,0 +1,92 @@
+package com.fastkart.sellerservice.controller;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fastkart.sellerservice.SellerServiceApplication;
+import com.fastkart.sellerservice.model.Product;
+import com.fastkart.sellerservice.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SellerServiceApplication.class)
+@WebAppConfiguration
+@AutoConfigureMockMvc
+class ControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+    @Test
+    void sellerViewProducts() throws Exception {
+
+        String uri = "http://localhost:8761/seller/viewProducts";
+
+        User user = new User();
+        user.setUsername("seller");
+
+        String inputJson = mapToJson(user);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+    }
+
+    @Test
+    void sellerAddProduct() throws Exception {
+        String uri = "http://localhost:8081/seller/addProducts";
+
+        Product product = new Product();
+        product.setUsername("seller");
+        product.setName("hp laptop");
+        product.setCategory("computers");
+        product.setDescription("hp windows laptop");
+        product.setMin_bid_amt_seller(2000.0);
+
+        String inputJson = mapToJson(product);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+
+
+    }
+}
